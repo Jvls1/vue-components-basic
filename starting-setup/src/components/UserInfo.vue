73b054c87b2f436adde856034086ae24b2c1dd42
João Vitor <joaovlopes000@gmail.com>
2022-07-11 21:33:03 -0300

    first commit

---
+++ starting-setup/src/components/UserInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <base-card>
+      <template #header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+      <p>{{ infoText }}</p>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import BaseCard from "@/components/UI/BaseCard";
+
+export default {
+  components: {BaseCard},
+  props: ['fullName', 'infoText', 'role'],
+};
+</script>
---
+++ vue-cli-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend:
+        {
+          id: 'joao',
+          name: 'João',
+          phone: '00 0000 0000',
+          email: 'joao@localhost.com'
+        }
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
